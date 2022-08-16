b082908d186727ac47df0688431b88df9c34fa83
Baisalya <baishalya1999@gmail.com>
2022-08-16 13:03:15 +0530

    login function add validator regex and async in main function

--- lib/Route/LoginScreen.dart
+++ lib/Route/LoginScreen.dart
@@ -1,6 +1,12 @@
+//import 'dart:html';
+//import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:homerental/Route/HomePage.dart';
 import 'package:homerental/signin.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -15,12 +21,24 @@ class _LoginScreenState extends State<LoginScreen> {
   //controlller
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController=new TextEditingController();
+  //firebase authentication
+  final auth=FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
+    //EMAIL
     final emailField =TextFormField(
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
+      validator: (value){
+        if(value!.isEmpty){
+          return("Please enter your email");
+        }
+        if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)){
+          return("Please enter a Valid Email-id");
+        }
+      },
       onSaved: (value){
         emailController.text=value!;
       },
@@ -34,10 +52,29 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
 
+    
+    //password
     final passwordfiled=TextFormField(
       autofocus: false,
       controller: passwordController,
      //    keyboardType: TextInputType.visiblePassword,
+      obscureText:true,
+      validator:(value){
+        /** r'^
+            (?=.*[A-Z])       // should contain at least one upper case
+            (?=.*[a-z])       // should contain at least one lower case
+            (?=.*?[0-9])      // should contain at least one digit
+            (?=.*?[!@#\$&*~]) // should contain at least one Special character
+            .{8,}             // Must be at least 8 characters in length
+            $ **/
+        RegExp regExp=new RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+        if(value!.isEmpty){
+          return ("password is required for login");
+        }
+        if(!regExp.hasMatch(value)){
+          return ("please enter valid password(min. 8 character)");
+        }
+      },
       onSaved: (value){
         passwordController.text=value!;
       },
@@ -55,7 +92,9 @@ class _LoginScreenState extends State<LoginScreen> {
       borderRadius: BorderRadius.circular(36),
       color: Colors.deepOrange,
 
-      child: MaterialButton(onPressed: (){},
+      child: MaterialButton(onPressed: (){
+        login(emailController.text, passwordController.text);
+      },
       child: const Text("Login",style:TextStyle(fontWeight:FontWeight.bold,),),),
     );
     return Scaffold(
@@ -102,4 +141,18 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+  //login vali
+void login(String email,String password)async{
+    if(_formKey.currentState!.validate()){
+      await auth.signInWithEmailAndPassword(email: email, password: password).
+      then((uid) => {
+        Fluttertoast.showToast(msg: "login Successfully"),
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>HomePage())),
+      }).catchError((e)
+      {
+        Fluttertoast.showToast(msg: e!.message);
+          });
+
+    }
+}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:homerental/Route/LoginScreen.dart';
 import 'package:homerental/signin.dart';
-
-void main() {
+// here added asy firebase for not exception erroe
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
