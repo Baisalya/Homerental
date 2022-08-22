42795cfce9c0ef4903ef7209ac685e1fa4db6d79
Baisalya <baishalya1999@gmail.com>
2022-08-22 10:14:28 +0530

    drawe ui added

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../widgets/drawer.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,11 +12,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Rental"),
+      appBar: AppBar(title: Center(child: Text("Home Rental")),
       ),
-      drawer: Drawer(
-        child: ListView(),
-      ),
+      drawer: MyDrawer(),
+
     );
   }
 }
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,143 @@
+
+
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:homerental/Pages/HomePage.dart';
+import 'package:homerental/model/user_model.dart';
+
+import '../Pages/LoginScreen.dart';
+//import 'package:flight_ticket/login_page.dart';
+
+class MyDrawer extends StatefulWidget {
+
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  //to show current user name details
+  User? user=FirebaseAuth.instance.currentUser;
+  UserModel userModel=UserModel();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseFirestore.instance.collection("users")
+    .doc(user!.uid)
+    .get()
+    .then((value){
+      this.userModel=UserModel.fromMap(value.data());
+    setState(() {});
+    });
+  }
+
+  @override
+  Widget drawertheme(){
+   return Container(decoration: const BoxDecoration(
+      gradient:LinearGradient(
+          colors: [
+            Colors.deepPurpleAccent,
+            Colors.indigoAccent,
+            Colors.cyanAccent,
+            Colors.cyan,
+            Colors.teal],
+          begin:Alignment.bottomCenter,
+          end:Alignment.topRight
+      ),
+
+    ),
+
+
+
+   );
+  }
+
+Widget builddraw(){
+
+
+
+  final image="Assets/image/drawer.jpg";
+  return Column(
+        children:<Widget>[ UserAccountsDrawerHeader(
+
+        margin: EdgeInsets.zero,
+        decoration: const BoxDecoration(
+          image:DecorationImage(image:const AssetImage("Assets/BG.jpg"),fit: BoxFit.cover),
+          color:Colors.blue),
+        accountName: Text("${userModel.firstname}",style:const TextStyle(fontSize:20,fontWeight:FontWeight.bold,color:Colors.white),),
+        accountEmail:Text("${userModel.email}",style:const TextStyle(fontSize:20,color:Colors.white),),
+        currentAccountPicture:CircleAvatar(backgroundImage:AssetImage(image)),
+        ),
+    ],
+
+
+    );//side of tringle
+}
+
+  @override
+  Widget build(BuildContext context) {
+    final  image="Assets/image/drawer.jpg";
+    return Drawer(
+      //child: Container(
+        child: Center(
+          child: Column(
+            //mainAxisAlignment: MainAxisSize.max,
+            children: <Widget>[
+              builddraw(),
+              const ListTile(
+                leading: const Icon(CupertinoIcons.home,color:Colors.blue,),title: const Text("HoME"),
+              //  onTap: (){Navigator.pushNamed(context,Myroutes.homeRoute);},
+              ),
+
+              ListTile(
+                  leading:const Icon(CupertinoIcons.profile_circled,color:Colors.blue),title:const Text("PrOFILE"),onTap: (){}),
+
+              const ListTile(
+                  leading:Icon(CupertinoIcons.bed_double_fill,color:Colors.blue),title:Text("Your Room"),
+                //  onTap: (){Navigator.pushNamed(context, Myroutes.ticketpage);}
+              ),
+
+              ListTile(
+                  leading:const Icon(CupertinoIcons.circle_grid_hex,color:Colors.blue),title:const Text("SETTING"),onTap: (){}),
+              ListTile(
+                  leading:const Icon(Icons.help_outline,color:Colors.blue),title:const Text("Need Help"),onTap: (){}),
+              const ListTile(
+                leading:Icon(Icons.money,color:Colors.blue),title:Text("Your Wallet"),
+                //  onTap: (){Navigator.pushNamed(context, Myroutes.paymentpage);}
+              ),
+              const Divider(),
+              Column(
+                children: <Widget>[
+                  const Text("Are You House owner",style:TextStyle(fontSize:20,fontWeight:FontWeight.bold),),
+                  ListTile(leading:const Icon(Icons.home_work_outlined),
+                    title:const Text("Give your House Detail"),onTap: (){},
+                  ),
+                ],
+              ),
+
+              Expanded(child: Align(alignment: Alignment.bottomLeft,
+                child: ListTile(
+                    leading:const Icon(Icons.logout,color:Colors.blue),title:const Text("Logout"),onTap: (){logout(context);},
+
+                ),
+              )),
+            ],
+
+
+                ),
+        ),
+
+          );
+
+  }
+  Future <void>logout(BuildContext context)async{
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder:(context)=>LoginScreen()));
+  }
+}
